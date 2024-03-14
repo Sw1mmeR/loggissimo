114dc930a369336c5807a398249cd1caa4424a87
mihail.sedrakyan <mihail.sedrakyan@eltex.loc>
2024-03-14 11:12:30 +0700

    Change user message style to BOLD

--- style.py
+++ style.py
@@ -42,86 +42,72 @@ class Style:
 
     def __init__(
         self,
-        inst_name_style: Tuple[Color, FontStyle, Color] = (Color.YELLOW, FontStyle.DEFAULT, Color.DEFAULT),
-        time_style: Tuple[Color, FontStyle, Color] = (Color.BLUE, FontStyle.DEFAULT, Color.DEFAULT),
+        inst_name_style: Tuple[Color, FontStyle, Color] = (
+            Color.YELLOW,
+            FontStyle.DEFAULT,
+            Color.DEFAULT,
+        ),
+        time_style: Tuple[Color, FontStyle, Color] = (
+            Color.BLUE,
+            FontStyle.DEFAULT,
+            Color.DEFAULT,
+        ),
         levelname_fontstyle: FontStyle = FontStyle.BOLD,
         level_style: Dict[Level, Tuple[Color, FontStyle, Color]] = {},
     ) -> None:
-        """
-        
-        """
+        """ """
         # namedtuple for style definitions
         style = namedtuple("style", ["text_color", "font_style", "background_color"])
         # instance name style
         self.inst_name = style(
-            inst_name_style[0].value,
-            inst_name_style[1].value, 
-            inst_name_style[2].value
-            )
+            inst_name_style[0].value, inst_name_style[1].value, inst_name_style[2].value
+        )
         # time part style
-        self.time = style(
-            time_style[0].value, 
-            time_style[1].value,
-            time_style[2].value
-            )
+        self.time = style(time_style[0].value, time_style[1].value, time_style[2].value)
         # styles for each level
-        self.level: dict = {}
+        self.level: Dict[Level:Tuple] = {}
 
         _trace_style = level_style.get(
-            Level.TRACE, (Color.CYAN, FontStyle.DEFAULT, Color.DEFAULT)
-            )
+            Level.TRACE, (Color.CYAN, FontStyle.BOLD, Color.DEFAULT)
+        )
         self.level[Level.TRACE] = style(
-            _trace_style[0].value, 
-            _trace_style[1].value,
-            _trace_style[2].value
-            )   
+            _trace_style[0].value, _trace_style[1].value, _trace_style[2].value
+        )
         _debug_style = level_style.get(
-            Level.DEBUG, (Color.BLUE, FontStyle.DEFAULT, Color.DEFAULT)
-            )
+            Level.DEBUG, (Color.BLUE, FontStyle.BOLD, Color.DEFAULT)
+        )
         self.level[Level.DEBUG] = style(
-            _debug_style[0].value, 
-            _debug_style[1].value,
-            _debug_style[2].value
-            )
+            _debug_style[0].value, _debug_style[1].value, _debug_style[2].value
+        )
         _info_style = level_style.get(
-            Level.INFO, (Color.WHITE, FontStyle.DEFAULT, Color.DEFAULT)
-            )
+            Level.INFO, (Color.WHITE, FontStyle.BOLD, Color.DEFAULT)
+        )
         self.level[Level.INFO] = style(
-            _info_style[0].value, 
-            _info_style[1].value,
-            _info_style[2].value
-            )
+            _info_style[0].value, _info_style[1].value, _info_style[2].value
+        )
         _success_style = level_style.get(
-            Level.SUCCESS, (Color.GREEN, FontStyle.DEFAULT, Color.DEFAULT)
+            Level.SUCCESS, (Color.GREEN, FontStyle.BOLD, Color.DEFAULT)
         )
         self.level[Level.SUCCESS] = style(
-            _success_style[0].value, 
-            _success_style[1].value,
-            _success_style[2].value
+            _success_style[0].value, _success_style[1].value, _success_style[2].value
         )
         _warn_style = level_style.get(
-            Level.WARNING, (Color.YELLOW, FontStyle.DEFAULT, Color.DEFAULT)
-            )
+            Level.WARNING, (Color.YELLOW, FontStyle.BOLD, Color.DEFAULT)
+        )
         self.level[Level.WARNING] = style(
-            _warn_style[0].value, 
-            _warn_style[1].value,
-            _warn_style[2].value
-            )
+            _warn_style[0].value, _warn_style[1].value, _warn_style[2].value
+        )
         _error_style = level_style.get(
-            Level.ERROR, (Color.RED, FontStyle.DEFAULT, Color.DEFAULT)
-            )
+            Level.ERROR, (Color.RED, FontStyle.BOLD, Color.DEFAULT)
+        )
         self.level[Level.ERROR] = style(
-            _error_style[0].value, 
-            _error_style[1].value,
-            _error_style[2].value
-            )
+            _error_style[0].value, _error_style[1].value, _error_style[2].value
+        )
         _crit_style = level_style.get(
-            Level.CRITICAL, (Color.MAGENTA, FontStyle.DEFAULT, Color.RED)
+            Level.CRITICAL, (Color.MAGENTA, FontStyle.BOLD, Color.RED)
         )
         self.level[Level.CRITICAL] = style(
-            _crit_style[0].value, 
-            _crit_style[1].value,
-            _crit_style[2].value
-            )
+            _crit_style[0].value, _crit_style[1].value, _crit_style[2].value
+        )
         # style for level name
         self.levelname_fstyle = levelname_fontstyle.value
